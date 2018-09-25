1157ec8da2c866fa75524a3062864dc613af93fb
Pedro Coca <pedro.coca@gmail.com>
2018-09-25 15:42:17 +0100

    Added a Load Balancer (ELB) and its security group.

--- main.tf
+++ main.tf
@@ -19,7 +19,11 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  
+  # The ASG resource needs a load_blancers parameter to register each instance in the ELB
+  load_balancers       = ["${aws_elb.example.name}"]
+  health_check_type    = "ELB"
 
   min_size = 2
   max_size = 5
@@ -51,3 +55,48 @@ variable "server_port" {
   default = 8080
 }
 
+# Adding Load Balancers to the configuration
+resource "aws_elb" "example" {
+  name		     = "terraform-asg-example"
+  availability_zones = ["{$data.aws_availability_zones.all.names}"]
+  security_groups = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port	  = 80
+    lb_protocol	  = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol= "http"
+  }
+
+  health_check {
+    healthy_threshold      = 2
+    unhealthy_threshold    = 2
+    timeout                = 3
+    interval               = 30
+    target                 = "HTTP:${var.server_port}/" 
+  }
+
+}
+
+# Adding the Security Group for the Load Balancer
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
+}
