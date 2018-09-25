f44831d12f3ddfec8e926debdaafe5db088bb495
Pedro Coca <pedro.coca@gmail.com>
2018-09-25 12:27:56 +0100

    Server port set as a variable

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
 		#!/bin/bash
 		echo "Hola Manola" > index.html
-		nohup busybox httpd -f -p 8080 &
+		nohup busybox httpd -f -p "${var.server_port}" &
 		EOF
 
   tags {
@@ -18,11 +18,16 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = "8080"
-    to_port = "8080"
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+variable "server_port" {
+  description = "The port of the server for the HTTP request"
+  default = 8080
 }
 
 output "public_ip" {
